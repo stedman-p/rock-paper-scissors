1fcc4ec90df9c0ccd1e7b1dbf173937161094fab
Peter Stedman <peterfstedman@gmail.com>
2022-07-20 11:01:32 -0400

    prettify scripts.js

--- scripts.js
+++ scripts.js
@@ -6,52 +6,52 @@
 
 const playbutton = document.getElementById("play");
 
-playbutton.addEventListener("click", function(e) {
+playbutton.addEventListener("click", function (e) {
     playRound();
 });
 
 function playRound() {
-
     // Prevents round if no option is selected
     if (!isRadioSelected()) {
         alert("Select a move!");
         return;
     }
 
-
-    const playerSelection = document.querySelector('input[name="rps-choice"]:checked').value;
+    const playerSelection = document.querySelector(
+        'input[name="rps-choice"]:checked'
+    ).value;
     const CPUSelection = computerPlay();
 
     const choiceDisplay = document.querySelectorAll(".choice");
     choiceDisplay.helloWorld = "Hello, World";
 
-    updateChoiceImage("choice-player",playerSelection);
-    updateChoiceImage("choice-cpu",CPUSelection);
+    updateChoiceImage("choice-player", playerSelection);
+    updateChoiceImage("choice-cpu", CPUSelection);
 
-    const gameResult = determineWinner(convertChoice(playerSelection), convertChoice(CPUSelection));
-    
-    document.getElementById("result-p").textContent = 
-        gameResult[0].toUpperCase() + gameResult.substring(1) + '!';
+    const gameResult = determineWinner(
+        convertChoice(playerSelection),
+        convertChoice(CPUSelection)
+    );
+
+    document.getElementById("result-p").textContent =
+        gameResult[0].toUpperCase() + gameResult.substring(1) + "!";
 
     incrementCounterById("round-p");
-    if (gameResult === "win")
-        incrementCounterById("wins-p");
-    else if (gameResult === "loss")
-        incrementCounterById("losses-p");
-    else
-        incrementCounterById("ties-p");
+    if (gameResult === "win") incrementCounterById("wins-p");
+    else if (gameResult === "loss") incrementCounterById("losses-p");
+    else incrementCounterById("ties-p");
 }
 
 function updateChoiceImage(elementId, newChoice) {
-    const choiceImg = document.getElementById(elementId);   
+    const choiceImg = document.getElementById(elementId);
     switch (newChoice) {
-        case 'r':
+        case "r":
             choiceImg.src = "./img/rock-icon.svg";
             break;
-        case 'p':
+        case "p":
             choiceImg.src = "./img/paper-icon.svg";
             break;
-        case 's':
+        case "s":
             choiceImg.src = "./img/scissors-icon.svg";
             break;
         default:
@@ -60,15 +60,13 @@ function updateChoiceImage(elementId, newChoice) {
 }
 
 function isRadioSelected() {
-    if (document.getElementById("rock-radio").checked ||
+    if (
+        document.getElementById("rock-radio").checked ||
         document.getElementById("paper-radio").checked ||
-        document.getElementById("scissors-radio").checked) {
-        
+        document.getElementById("scissors-radio").checked
+    ) {
         return true;
-    }
-    else
-        return false;
-    
+    } else return false;
 }
 
 function incrementCounterById(elementId) {
