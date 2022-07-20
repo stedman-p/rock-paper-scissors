8cdbec0fd7f92eeed659a87ea229512476ea4888
Peter Stedman <peterfstedman@gmail.com>
2022-07-20 00:27:23 -0400

    Implement game

--- scripts.js
+++ scripts.js
@@ -4,67 +4,70 @@
     rps = the popular game Rock Paper Scissors
 */
 
-const playerChoices = document.querySelectorAll("input");
-
-playerChoices.forEach(element => {
-    element.addEventListener("click", selectChoice)
-});
-
 const playbutton = document.getElementById("play");
 
 playbutton.addEventListener("click", function(e) {
-    incrementCounterById("round-p")
+    playRound();
 });
 
-function game() {
-    let winLoseDraw = [0, 0, 0];
-    let winString = "";
-    let roundString = [];
-    for (i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        roundString = playRound(
-            // change input to buttons
-            prompt("'Rock', 'Paper', or 'Scissors?'"),
-            computerPlay()
-        );
-        winString = roundString.split(" ")[1];
-        console.log(roundString);
-        if (winString === "win!") {
-            winLoseDraw[0]++;
-        } else if (winString === "lose!") {
-            winLoseDraw[1]++;
-        } else {
-            winLoseDraw[2]++;
-        }
+function playRound() {
+
+    // Prevents round if no option is selected
+    if (!isRadioSelected()) {
+        alert("Select a move!");
+        return;
     }
 
-    console.log(
-        `Wins: ${winLoseDraw[0]}, Losses: ${winLoseDraw[1]}, Draws: ${winLoseDraw[2]}`
-    );
-    if (winLoseDraw[0] > winLoseDraw[1]) {
-        console.log("You win!");
-    } else if (winLoseDraw[0] < winLoseDraw[1]) {
-        console.log("You Lose!");
-    } else {
-        console.log("It's a TIE!");
+
+    const playerSelection = document.querySelector('input[name="rps-choice"]:checked').value;
+    const CPUSelection = computerPlay();
+
+    const choiceDisplay = document.querySelectorAll(".choice");
+    choiceDisplay.helloWorld = "Hello, World";
+
+    updateChoiceImage("choice-player",playerSelection);
+    updateChoiceImage("choice-cpu",CPUSelection);
+
+    const gameResult = determineWinner(convertChoice(playerSelection), convertChoice(CPUSelection));
+    
+    document.getElementById("result-p").textContent = 
+        gameResult[0].toUpperCase() + gameResult.substring(1) + '!';
+
+    incrementCounterById("round-p");
+    if (gameResult === "win")
+        incrementCounterById("wins-p");
+    else if (gameResult === "loss")
+        incrementCounterById("losses-p");
+    else
+        incrementCounterById("ties-p");
+}
+
+function updateChoiceImage(elementId, newChoice) {
+    const choiceImg = document.getElementById(elementId);   
+    switch (newChoice) {
+        case 'r':
+            choiceImg.src = "./img/rock-icon.svg";
+            break;
+        case 'p':
+            choiceImg.src = "./img/paper-icon.svg";
+            break;
+        case 's':
+            choiceImg.src = "./img/scissors-icon.svg";
+            break;
+        default:
+            break;
     }
 }
 
-    // Returns 0 if the player wins, 1 if the player loses, 3 if they tie
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    winStatus = determineWinner(
-        convertChoice(playerSelection),
-        convertChoice(computerSelection)
-    );
-
-    return `You ${winStatus}! ${playerSelection.toUpperCase()} ${winStatus}s against ${computerSelection.toUpperCase()}!`;
-}
-
-function selectChoice() {
+function isRadioSelected() {
+    if (document.getElementById("rock-radio").checked ||
+        document.getElementById("paper-radio").checked ||
+        document.getElementById("scissors-radio").checked) {
+        
+        return true;
+    }
+    else
+        return false;
     
 }
 
@@ -76,14 +79,14 @@ function incrementCounterById(elementId) {
 
 function determineWinner(player1, player2) {
     /*
-       params: converted player rps choice
+       params: 
        returns: winner of round from player1's perspective
     */
 
     if (player1 === player2) {
         return "tie";
     } else if ((player1 + 1) % 3 === player2) {
-        return "lose";
+        return "loss";
     } else {
         return "win";
     }
@@ -91,16 +94,16 @@ function determineWinner(player1, player2) {
 
 function convertChoice(rpsChoice) {
     /* 
-        params: string of 'rock', 'paper', or 'scissors'
+        params: string of 'r', 'p', or 's'
         returns: number corresponding to string
         for use in rps algorithm from https://stackoverflow.com/a/41458257
     */
     switch (rpsChoice) {
-        case "rock":
+        case "r":
             return 0;
-        case "paper":
+        case "p":
             return 1;
-        case "scissors":
+        case "s":
             return 2;
     }
 }
@@ -110,10 +113,10 @@ function computerPlay() {
     const guess = Math.floor(Math.random() * 3) + 1;
     switch (guess) {
         case 1:
-            return "rock";
+            return "r";
         case 2:
-            return "paper";
+            return "p";
         case 3:
-            return "scissors";
+            return "s";
     }
 }
